1d86adfc328df86ae7f448b0a693396d3ba65071
Epinder <epinder.singh@gmail.com>
2021-06-30 17:02:39 +0800

    interfaces test

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,18 +1,19 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
+  const y = vehicle.year.getUTCFullYear();
   console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
+  console.log(`Year: ${y}`);
   console.log(`Broken: ${vehicle.broken}`);
 };
 
