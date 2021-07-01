1e514df94a7e7365829f360cc0783ef7cd2b292e
Epinder <epinder.singh@gmail.com>
2021-07-01 16:54:36 +0800

    trying to fix bug

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,12 @@
-/// <reference types="@types/google.maps" />
+/// <reference types="@types/google-maps" />
 
-import { User } from './components/User';
-import { Company } from './components/Company';
+// import { User } from './components/User';
+// import { Company } from './components/Company';
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
