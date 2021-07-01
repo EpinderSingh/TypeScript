00c200dd2d032b8b246ad03c5120b166d5c3da28
Epinder <epinder.singh@gmail.com>
2021-07-01 11:42:22 +0800

    Comapny file created

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,8 @@
 import { User } from './components/User';
+import { Company } from './components/Company';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
