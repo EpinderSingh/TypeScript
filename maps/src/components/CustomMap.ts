452c32cf0ab8e31ebd3ee8093a4226b655d99cc8
Epinder <epinder.singh@gmail.com>
2021-07-02 10:57:56 +0800

    adding marker on map

--- maps/src/components/CustomMap.ts
+++ maps/src/components/CustomMap.ts
@@ -1,7 +1,12 @@
 /// <reference types="@types/google.maps" />
 
-import { User } from './User';
-import { Company } from './Company';
+// Instruction to every other class on how they can be an argument to 'addMarker
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -16,7 +21,7 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -25,14 +30,4 @@ export class CustomMap {
       },
     });
   }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng,
-  //     },
-  //   });
-  // }
 }
